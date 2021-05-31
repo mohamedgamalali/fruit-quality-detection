adba656860ecd812d7e99bad323db1630a09d3c2
ahmed2929 <mywork304@gmail.com>
2021-05-31 04:37:03 +0200

    remove ai

---
+++ ai.js
@@ -0,0 +1,79 @@
+const tf = require("@tensorflow/tfjs")
+const tfnode = require("@tensorflow/tfjs-node")
+const MODEL_CLASSES = {
+  0: 'Fresh Apple',
+  1: 'Fresh Banana',
+  2: 'Fresh Orange',
+  3: 'Rotten Apple',
+  4: 'Rotten Banana',
+  5: 'Rotten Orange'
+};
+
+const MODEL_PATH = 'http://localhost:3000/model/model.json';
+const IMAGE_SIZE = 150;
+const CANVAS_SIZE = 224;
+const TOPK_PREDICTIONS = 3;
+
+
+processImage = async (image) => {
+  return tf.tidy(() => image.expandDims(0).toFloat().div(127).sub(1));
+}
+
+
+
+
+classifyLocalImage = async (buffer) => {
+  try {
+
+    const model = await tf.loadLayersModel(MODEL_PATH);
+    const image = tf.tidy(() => tfnode.node.decodeImage(buffer))
+
+    // Process and resize image before passing in to model.
+    const imageData = await processImage(image);
+    console.debug("image is : ", imageData)
+    //const resizedImage = tf.image.resizeBilinear(imageData, [IMAGE_SIZE, IMAGE_SIZE]);
+    resizedImage = imageData
+
+    const logits = model.predict(resizedImage);
+    const probabilities = await logits.data();
+    const preds = await getTopKClasses(probabilities, TOPK_PREDICTIONS);
+    return preds
+
+    // Dispose of tensors we are finished with.
+    image.dispose();
+    imageData.dispose();
+    resizedImage.dispose();
+    logits.dispose();
+  } catch (err) {
+    console.debug(err)
+  }
+}
+
+
+getTopKClasses = async (values, topK) => {
+  const valuesAndIndices = [];
+  for (let i = 0; i < values.length; i++) {
+    valuesAndIndices.push({ value: values[i], index: i });
+  }
+  valuesAndIndices.sort((a, b) => {
+    return b.value - a.value;
+  });
+  const topkValues = new Float32Array(topK);
+  const topkIndices = new Int32Array(topK);
+  for (let i = 0; i < topK; i++) {
+    topkValues[i] = valuesAndIndices[i].value;
+    topkIndices[i] = valuesAndIndices[i].index;
+  }
+
+  const topClassesAndProbs = [];
+  for (let i = 0; i < topkIndices.length; i++) {
+    topClassesAndProbs.push({
+      className: MODEL_CLASSES[topkIndices[i]],
+      probability: (topkValues[i] * 100).toFixed(2)
+    });
+  }
+  return topClassesAndProbs;
+}
+
+
+module.exports = classifyLocalImage
---
+++ app.js
@@ -0,0 +1,104 @@
+const app = require("express")()
+const express = require("express")
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const multer = require('multer')
+const fs = require('fs');
+const path = require('path')
+const classify = require("./ai")
+const sharp = ("sharp")
+
+
+require('dotenv').config();
+
+
+const port = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+
+
+
+    }
+
+
+
+})
+
+var checkImage = function (file, cb) {
+
+
+    var ext = path.extname(file.originalname);
+
+    if (ext === '.png' || ext === '.jpg' || ext === '.jpeg') {
+        cb(null, true)
+    } else {
+        cb('not an image', false)
+    }
+
+
+}
+
+
+var upload = multer({
+    storage: storage,
+    fileFilter: function (req, file, cb) {
+        checkImage(file, cb)
+    }
+})
+
+app.use(bodyParser.json());
+
+
+app.use('/model', express.static(path.join(__dirname, 'model')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+
+app.use(upload.any('img'));
+
+
+//routes
+
+const classifyRouter = require('./routes/classify');
+const relableRouter = require('./routes/relabel');
+
+//client
+const clientAuth = require('./routes/client/auth');
+
+//seller
+const sellerAuth = require('./routes/seller/auth');
+
+//app.use('/classify', classifyRouter);
+//app.use('/relabel', relableRouter);
+
+//clinet 
+app.use('/client', clientAuth);
+
+app.use('/seller', sellerAuth);
+
+app.use((error, req, res, next) => {
+    const status = error.statusCode || 500;
+    const state = error.state || 0;
+    const message = error.message;
+    res.status(status).json({ state: state, message: message });
+});
+
+
+mongoose
+    .connect(
+        MONGODB_URI, {
+        useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false
+    })
+    .then(result => {
+        app.listen(port);
+
+        //=======>>>>    //scadual section// <<<<<<=====
+    })
+    .catch(err => {
+        console.log(err);
+    });
